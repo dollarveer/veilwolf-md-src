780ab1e3ca624a5a4131f9a012a5ad4d3de052f1
dollarveer <85345503+dollarveer@users.noreply.github.com>
2025-07-15 20:55:45 +0300

    Update helper.js

--- Ibrahim/helper.js
+++ Ibrahim/helper.js
@@ -3,29 +3,9 @@ const BOT_NAME = "VEILWOLF_XMD";
 const DEFAULT_THUMBNAIL = "https://raw.githubusercontent.com/dollarveer/veilwolf-md-src/refs/heads/main/fotor_1752486419384.jpg";
 
 const createContext = (userJid, options = {}) => ({
-    contextInfo: {
-        mentionedJid: [userJid], // Tag user if needed
-        forwardingScore: 999,
-        isForwarded: true,
-        businessMessageForwardInfo: {
-            businessOwnerJid: NEWS_LETTER_JID, // Helps add verified feel
-        },
-        forwardedNewsletterMessageInfo: {
-            newsletterJid: NEWS_LETTER_JID,
-            newsletterName: options.newsletterName || BOT_NAME,
-            serverMessageId: Math.floor(100000 + Math.random() * 900000)
-        },
-        externalAdReply: {
-            title: options.title || BOT_NAME,
-            body: options.body || "Premium WhatsApp Bot Solution",
-            thumbnailUrl: options.thumbnail || DEFAULT_THUMBNAIL,
-            mediaType: 1,
-            mediaUrl: options.mediaUrl || undefined,
-            sourceUrl: options.sourceUrl || "https://wa.me/254702528705", // link to bot or business
-            showAdAttribution: true,
-            renderLargerThumbnail: false 
-        }
-    }
+  contextInfo: {
+    mentionedJid: [userJid]
+  }
 });
 
 module.exports = {
