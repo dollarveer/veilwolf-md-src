efc61bdb4e03895542192c7fc312de75411c47f9
dollarveer <85345503+dollarveer@users.noreply.github.com>
2025-07-15 18:14:49 +0300

    Update helper.js

--- Ibrahim/helper.js
+++ Ibrahim/helper.js
@@ -1,5 +1,5 @@
 const NEWS_LETTER_JID = "120363285388090068@newsletter"; // Replace with your real one
-const BOT_NAME = "BWM-XMD";
+const BOT_NAME = "VEILWOLF_XMD";
 const DEFAULT_THUMBNAIL = "https://raw.githubusercontent.com/dollarveer/veilwolf-md-src/refs/heads/main/fotor_1752486419384.jpg";
 
 const createContext = (userJid, options = {}) => ({
